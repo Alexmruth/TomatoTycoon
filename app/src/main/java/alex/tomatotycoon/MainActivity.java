73fcd0d0e05901bfdc15ca2aea6266e72eeb181d
Alexmruth <alexmruth@gmail.com>
2019-06-11 21:19:28 +1000

    commit

--- app/src/main/java/alex/tomatotycoon/MainActivity.java
+++ app/src/main/java/alex/tomatotycoon/MainActivity.java
@@ -1,55 +1,111 @@
 package alex.tomatotycoon;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView tomatoImg;
+    ImageButton tomatoImg, rainTest;
+    ImageView littleTomato;
     TextView countText;
     Button shopBtn;
+    ProgressBar rainProgress;
     int multiplier = 1;
     int tomatoes = 0;
+    int rainProgressInt = 0;
 
     // The Image uses 88% of the screen width
     private static final float IMAGE_SCREEN_WIDTH_RATIO = 0.60f;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         tomatoImg = findViewById(R.id.tomato);
+        littleTomato = findViewById(R.id.littleTomato);
         countText = findViewById(R.id.countText);
 
-        shopBtn = findViewById(R.id.shopBtn);
+        rainTest = findViewById(R.id.rainTest);
 
+        rainProgress = findViewById(R.id.rainProgress);
+        rainProgress.setMax(100);
+        shopBtn = findViewById(R.id.shopBtn);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;;
         int imageViewSize = (int) ((float) width * IMAGE_SCREEN_WIDTH_RATIO);
-        int margin = (width - imageViewSize) / 2;
-        ViewGroup.LayoutParams params = tomatoImg.getLayoutParams();
-        params.height = imageViewSize;
-        params.width = imageViewSize;
 
-        tomatoImg.requestLayout();
+        tomatoImg.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                ViewGroup.LayoutParams params = tomatoImg.getLayoutParams();
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    tomatoes = tomatoes + (1*multiplier);
+                    rainProgressInt = rainProgressInt + 1;
+                    countText.setText(String.valueOf(tomatoes));
+                    rainProgress.setProgress(rainProgressInt);
 
+                    if(rainProgressInt >= 100) {
+                        tomatoRain();
+                        rainProgressInt = 0;
+                        rainProgress.setProgress(rainProgressInt);
+                    }
+                    Log.d("TouchTest", "Touch down");
+                    params.height = params.height - 30;
+                    params.width = params.width - 30;
+                    view.setLayoutParams(params);
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    Log.d("TouchTest", "Touch up");
+                    params.height = params.height + 30;
+                    params.width = params.width + 30;
+                    view.setLayoutParams(params);
+                }
+                return true;
+            }
+        });
+
+//        tomatoImg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tomatoes = tomatoes + (1*multiplier);
+//                rainProgressInt = rainProgressInt + 1;
+//                countText.setText(String.valueOf(tomatoes));
+//                rainProgress.setProgress(rainProgressInt);
+//
+//                if(rainProgressInt >= 100) {
+//                    tomatoRain();
+//                    rainProgressInt = 0;
+//                    rainProgress.setProgress(rainProgressInt);
+//                }
+//                ViewGroup.LayoutParams params = tomatoImg.getLayoutParams();
+//                if(v.isPressed()) {
+//                    params.height = params.height - 30;
+//                    params.width = params.width - 30;
+//                }
+//            }
+//        });
     }
 
-    public void onTomatoClick(View v) {
-        tomatoes = tomatoes + (1*multiplier);
-        countText.setText(String.valueOf(tomatoes));
+    public void tomatoRain() {
     }
+
 
     public void shopBtnClick(View view) {
         startActivity(new Intent(MainActivity.this,ShopActivity.class));
--- app/src/main/java/alex/tomatotycoon/ShopActivity.java
+++ app/src/main/java/alex/tomatotycoon/ShopActivity.java
@@ -9,6 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.View;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 public class ShopActivity extends AppCompatActivity {
 
     @Override
@@ -24,6 +31,37 @@ public class ShopActivity extends AppCompatActivity {
 
        // getWindow().setLayout(width, height);
         getWindow().setLayout((int) (width*0.8),(int) (height*0.8));
+
+        try {
+            loadPlayerData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String loadGameData() {
+        String json = null;
+        try {
+            InputStream file = getAssets().open("gameData.json");
+            int size = file.available();
+            byte[] buffer = new byte[size];
+            file.read(buffer);
+            file.close();
+            json = new String(buffer, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public void loadPlayerData() throws JSONException {
+        JSONObject obj = new JSONObject(loadGameData());
+        JSONObject jo = obj.getJSONObject("Upgrades");
+        int base = jo.getInt("baseAttMin");
+        int baseAttMax = jo.getInt("baseAttMax");
     }
 
 }
